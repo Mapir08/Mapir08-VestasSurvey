ebff0b36f6a4a880b2df9e54a7098f01c9f466c9
MAPIR <math.perlier@gmail.com>
2021-05-23 23:58:10 +0200

    transfert de JS à PHP puis JS ok mais traitement JSON encore compliqué

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,76 @@
-$('#log').submit(function(e){
-  e.preventDefault();
-  let listeQuestion;
-  $.ajax({
-    type: 'POST',
-    url: 'public/php/log.php',
-    data: listeQuestion,
-    dataType: 'json',
-    success: function(result){
-      if(result.isSuccess){
-        // Confirmer que TOUTES les questions sont bien dans liste questions
-        // lancer la modification de l'HTML
-        // la première question
+$(function(){
+
+  let qNum, nbMaxQ;
+  let listeQuestions=[];
+
+  $('#log').submit(function(e){
+    e.preventDefault();
+    // RECUPERER LA LISTE DES QUESTION VOULU en vérifiant si le formulaire est correct
+    let logData = $('#log').serialize();
+    $.ajax({
+      type: 'POST',
+      url: 'public/php/log.php',
+      data: logData,
+      dataType: 'json',
+      success: function(liste){
+        if(liste.isSuccess){
+          // Confirmer que TOUTES les questions sont bien dans liste questions
+            console.log(liste); // ATTENTION c'est un tableau JSON
+            
+            
+            
+            console.log(listeQuestions);
+            qNum = 0;
+            nbMaxQ = Object.keys(liste).length;
+            afficherPage("questionnaire");
+            remplissageQuestionnaire();
+          // la première question
+        } else {
+          $('#log_code').addClass('error');
+        }
       }
+    });
+  });
+  $('#exam').submit(function(e){
+    e.preventDefault();
+    // CONTROLER SI UN CHOIX A ETE FAIT
+
+    // ENREGISTREMENT DE LA REPONSE
+
+    // QUESTION SUIVANTE
+    if (qNum < nbMaxQ) {
+      qNum += 1;
+      remplissageQuestionnaire()
+    } else {
+      afficherPage('resultat');
     }
   });
   
+  function afficherPage(laquelle) {
+    switch (laquelle) {
+      case 'questionnaire':
+        $('#login').remove();
+        $('#questionnaire').removeAttr('style');
+        break;
+      case 'resultat':
+        $('section').remove();
+        $('header').after('\
+        <section id="error" class="container-fluid">\
+          <div class="error-msg">FINI !</div>\
+          <a href="" class="btn btn-colored">Bravo</a>\
+        </section>'
+      );
+        break;
+    }
+  };
+  function remplissageQuestionnaire(){
+    $('.exam_question').text('Question 1 ?');
+    $('.exam_img').html('<img src="public/img/questions/Q'+ (qNum+1) +'.png">');
+    $('#choix1').next().text('Réponse A');
+    $('#choix2').next().text('Reponse B');
+    $('#choix3').next().text('Reponse C');
+    $('#choix4').next().text('Reponse D');
+    $('.exam_compteur').html('<span class="exam_compteur_num">' + (qNum+1) + '</span> / ' + nbMaxQ);
+  };
+
 });
--- public/php/log.php
+++ public/php/log.php
@@ -1,4 +1,68 @@
 <?php
 
+// LISTING DES QUESTIONS
+$Q1 = array(
+  'question' => "A quoi correspond le symbole ci-dessous ?",
+  'img' => true,
+  'reponse' => 4,
+  'choix1' => "Un contacteur",
+  'choix2' => "Un disjoncteur",
+  'choix3' => "Un fusible",
+  'choix4' => "Un transformateur"
+);
+$Q2 = array(
+  'question' => "Quel est le composant sur la photos ?",
+  'img' => true,
+  'reponse' => 3,
+  'choix1' => "Un contacteur",
+  'choix2' => "Un disjoncteur",
+  'choix3' => "Un relai thermique",
+  'choix4' => "Un transformateur"
+);
+$Q3 = array(
+  'question' => "A quoi correspond le symbole ci-dessous ?",
+  'img' => true,
+  'reponse' => 1,
+  'choix1' => "Un contacteur",
+  'choix2' => "Un disjoncteur",
+  'choix3' => "Un fusible",
+  'choix4' => "Un transformateur"
+);
+$Q4 = array(
+  'question' => "A quoi correspond le symbole ci-dessous ?",
+  'img' => true,
+  'reponse' => 2,
+  'choix1' => "Un contacteur",
+  'choix2' => "Un disjoncteur magnéto-thermique",
+  'choix3' => "Un fusible",
+  'choix4' => "Un transformateur"
+);
+$Q5 = array(
+  'question' => "Quel est la différence entre un relais et un contacteur ?",
+  'img' => false,
+  'reponse' => 2,
+  'choix1' => "Le contacteur permet de supporter des courants plus faibles",
+  'choix2' => "Le contacteur permet de supporter des courants plus forts"
+);
+
+$questionnaire = array(
+  'Q1' => $Q1,
+  'Q2' => $Q2,
+  'Q3' => $Q3,
+  'Q4' => $Q4,
+  'Q5' => $Q5,
+  'isSuccess' => false
+);
+
+// ACTION AU 'POST'
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+  $questionnaire['isSuccess'] = true;
+
+  if ($_POST['log_code'] != 'V23e7h'){
+    $questionnaire['isSuccess'] = false;
+  }
+  echo json_encode($questionnaire);
+}
 
 ?>
