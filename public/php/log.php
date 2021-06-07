d4d83429192992d0251374c5192458dfe56916b3
MAPIR <math.perlier@gmail.com>
2021-06-07 17:02:38 +0200

    saves

--- public/php/log.php
+++ public/php/log.php
@@ -2,11 +2,13 @@
 
 session_start();
 
-$_SESSION['nom'] = strtolower($_POST['accountForm_log']);
-$_SESSION['pass'] = $_POST['accountForm_pass'];
+if (strtolower($_POST['accountForm_log']) == 'vestas' && $_POST['accountForm_pass'] == 'Welcome123' ){
+  $_SESSION['nom'] = strtolower($_POST['accountForm_log']); // A CHANGER UNE FOIS SUR SERVEUR
+  $_SESSION['pass'] = $_POST['accountForm_pass'];           // A CHANGER UNE FOIS SUR SERVEUR
+}
 $_SESSION['table'] = 'savestest'; // A CHANGER UNE FOIS SUR SERVEUR
 $_SESSION['bdd'] = 'survey';      // A CHANGER UNE FOIS SUR SERVEUR
-$_SESSION['host'] = 'localhost';
+$_SESSION['host'] = 'localhost';  // A CHANGER UNE FOIS SUR SERVEUR
 
 // CONNEXION MYSQL
 try{
